9f79da2ed9203ca458ecb1aa3f93cd34cf89ab03
Brandon Harad <haradb@catlin.edu>
2025-09-23 14:25:21 -0700

    feat: definable variables

--- main.hs
+++ main.hs
@@ -39,71 +39,74 @@ vapp (VLam f) v = f v
 vapp (VNeutral n) v = VNeutral (NApp n v)
 
 type Env = [Value]
+type Equalities = [(Name, CheckableTerm)]
 
-evalInferable' :: InferableTerm -> Env -> Value
-evalInferable' (Ann t _) e = evalCheckable' t e
-evalInferable' (Bound i) e = e !! i
-evalInferable' (Free n) e = vfree n
-evalInferable' (App f x) e = vapp (evalInferable' f e) (evalCheckable' x e)
-evalInferable' (Univ n) e = VUniv n
-evalInferable' (Fun t t') e = VFun (evalCheckable' t e) (evalCheckable' t' e)
+evalInferable' :: Env -> Equalities -> InferableTerm -> Value
+evalInferable' env eqs (Ann t _) = evalCheckable' env eqs t
+evalInferable' env eqs (Bound i) = env !! i
+evalInferable' env eqs (Free n) = case lookup n eqs of
+        Just t -> evalCheckable' env eqs t
+        Nothing -> vfree n
+evalInferable' env eqs (App f x) = vapp (evalInferable' env eqs f) (evalCheckable' env eqs x)
+evalInferable' env eqs (Univ n) = VUniv n
+evalInferable' env eqs (Fun t t') = VFun (evalCheckable' env eqs t) (evalCheckable' env eqs t')
 
 evalInferable :: InferableTerm -> Value
-evalInferable t = evalInferable' t []
+evalInferable = evalInferable' [] []
 
-evalCheckable' :: CheckableTerm -> Env -> Value
-evalCheckable' (Lam f) e = VLam (\x -> evalCheckable' f (x : e))
-evalCheckable' (Inf t) e = evalInferable' t e
+evalCheckable' :: Env -> Equalities -> CheckableTerm -> Value
+evalCheckable' env eqs (Lam f) = VLam (\x -> evalCheckable' (x : env) eqs f)
+evalCheckable' env eqs (Inf t) = evalInferable' env eqs t
 
 evalCheckable :: CheckableTerm -> Value
-evalCheckable t = evalCheckable' t []
+evalCheckable = evalCheckable' [] []
 
 type Judgements = [(Name, CheckableTerm)]
-type Equalities = [(Name, CheckableTerm)]
 
 type Result a = Either String a
 throwError = Left
 
-inferType :: Judgements -> InferableTerm -> Result CheckableTerm
+inferType :: Judgements -> Equalities -> InferableTerm -> Result CheckableTerm
 inferType = inferType' 0
 
-inferType' :: Int -> Judgements -> InferableTerm -> Result CheckableTerm
+inferType' :: Int -> Judgements -> Equalities -> InferableTerm -> Result CheckableTerm
 -- If we receive an annotated type, just check if the annotation works
-inferType' i judgements (Ann e t) = do
-    checkType' i judgements e t
+inferType' i judgements eqs (Ann e t) = do
+    checkType' i judgements eqs e t
     return t
-inferType' i judgements (Free x) = case
-    lookup x judgements of
+inferType' i judgements eqs (Free x) = case lookup x eqs of
         Just t -> return t
-        Nothing -> throwError "unknown identifier"
-inferType' i judgements (App e e') = do
-    t <- inferType' i judgements e
+        Nothing -> case lookup x judgements of
+            Just t -> return t
+            Nothing -> throwError "unknown identifier"
+inferType' i judgements eqs (App e e') = do
+    t <- inferType' i judgements eqs e
     case t of
-        Inf (Fun a b) -> do checkType' i judgements e' a; return b
+        Inf (Fun a b) -> do checkType' i judgements eqs e' a; return b
         _ -> throwError "illegal application"
-inferType' i judgements (Univ n) = 
+inferType' i judgements eqs (Univ n) =
     if n < 0 then throwError "Universe indices must be nonnegative"
     else Right (Inf (Univ (n+1)))
-inferType' i judgements (Fun t t') = case (t, t') of
+inferType' i judgements eqs (Fun t t') = case (t, t') of
     (Inf u, Inf u') -> do
-        v <- inferType judgements u
-        v' <- inferType judgements u'
+        v <- inferType' i judgements eqs u
+        v' <- inferType' i judgements eqs u'
         case (v, v') of
             (Inf (Univ j), Inf (Univ k)) -> Right (Inf (Univ (max j k)))
             _ -> throwError "improperly formed function type"
     _ -> throwError "improperly formed function type"
-inferType' _ _ (Bound t) = throwError "something has gone horribly wrong"
+inferType' _ _ _ (Bound t) = throwError "something has gone horribly wrong"
 
-checkType :: Judgements -> CheckableTerm -> CheckableTerm -> Result ()
+checkType :: Judgements -> Equalities -> CheckableTerm -> CheckableTerm -> Result ()
 checkType = checkType' 0
 
-checkType' :: Int -> Judgements -> CheckableTerm -> CheckableTerm -> Result ()
-checkType' i judgements (Inf e) t = do
-    t' <- inferType' i judgements e
+checkType' :: Int -> Judgements -> Equalities -> CheckableTerm -> CheckableTerm -> Result ()
+checkType' i judgements eqs (Inf e) t = do
+    t' <- inferType' i judgements eqs e
     unless (t == t') (throwError "incorrect annotation")
-checkType' i judgements (Lam e) (Inf (Fun t t')) =
-    checkType' (i+1) ((Local i, t) : judgements) (substCheckable 0 (Free (Local i)) e) t'
-checkType' i judgements _ _ = throwError "invalid typing"
+checkType' i judgements eqs (Lam e) (Inf (Fun t t')) =
+    checkType' (i+1) ((Local i, t) : judgements) eqs (substCheckable 0 (Free (Local i)) e) t'
+checkType' _ _ _ _ _ = throwError "invalid typing"
 
 substInferable :: Int -> InferableTerm -> InferableTerm -> InferableTerm
 substInferable i r (Ann e t) = Ann (substCheckable i r e) t
@@ -132,6 +135,9 @@ neutralQuote i (NApp n v) = App (neutralQuote i n) (quote' i v)
 
 assume :: String -> CheckableTerm -> Judgements -> (InferableTerm, Judgements)
 assume s t j = (Free (Global s), (Global s, t) : j)
+
+define :: String -> CheckableTerm -> Equalities -> (InferableTerm, Equalities)
+define s t e = (Free (Global s), (Global s, t) : e)
 
 main :: IO ()
 main = do
